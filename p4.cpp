af171c7b0ae317e0c8dabdaea119cce6dcc547a7
Abhisek Ray <66687450+Abhisek-Ray99@users.noreply.github.com>
2021-07-09 09:22:09 +0530

    Create p4.cpp

---
+++ p4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    if(cout<<"hello"){
+        cout<<"world";
+    }else{
+        cout<<"India";
+    }
+
+    return 0;
+}
+
+
+output --------> helloworld
