93e624f6adb509105ed9f73584bf0908cf2d28ae
Abhisek Ray <66687450+Abhisek-Ray99@users.noreply.github.com>
2021-07-10 10:17:35 +0530

    Create p27.cpp

---
+++ p27.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a[][] = {{1,2},{3,4}};
+    int i,j;
+    for(int i=0; i<2; i++){
+        for(int j=0; j<2; j++){
+            cout<<a[i][j]<<endl;
+        }
+    }
+
+    return 0;
+}
+
+
+
+output --------> eror
