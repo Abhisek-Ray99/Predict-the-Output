1f41244d31c6731e3b1f1bc8f437bdfecc854377
Abhisek Ray <66687450+Abhisek-Ray99@users.noreply.github.com>
2021-07-10 09:22:16 +0530

    Create p11.cpp

---
+++ p11.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    enum{a=90,b,c=8,d};
+    cout<<a<<" "<<b<<" "<<c<<" "<<d;
+
+    return 0;
+}
+
+
+
+output -----------------> 90 91 8 9
